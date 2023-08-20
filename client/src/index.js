cfeabf90d5814fd15bfd98c60ccf4c5f3f3ebe75
Hakateeee <142707796+Hakateeee@users.noreply.github.com>
2023-08-20 11:36:15 +0700

    Update index.js
    Test

--- client/src/index.js
+++ client/src/index.js
@@ -4,3 +4,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+//Đây chỉ là test việc vận hành của GIT có hoạt động hay không
